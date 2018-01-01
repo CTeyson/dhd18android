d0ee0754ec796bcc4d3ec924d4a64c7e5bc16200
marcozeyen <marco.zeyen@gmail.com>
2018-01-01 17:46:27 +0100

    Removed keys for accessing ConfTool.

--- app/src/main/java/com/unikoeln/mazey/dhdexamplesecond/activities/fragments/WorkInProgressFragment.java
+++ app/src/main/java/com/unikoeln/mazey/dhdexamplesecond/activities/fragments/WorkInProgressFragment.java
@@ -10,29 +10,17 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.unikoeln.mazey.dhdexamplesecond.R;
-import com.unikoeln.mazey.dhdexamplesecond.activities.data.eventdata.Session;
 
-import java.util.List;
-import java.util.Observable;
-import java.util.Observer;
-
-// TODO Access data via "singleton object"
-
-public class WorkInProgressFragment extends Fragment implements Observer {
+public class WorkInProgressFragment extends Fragment {
 
     private View view;
     private TextView textView;
-
-    boolean sessionsLoaded;
-
-    List<Session> sessions = null;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.work_in_progress_layout, container, false);
         textView = view.findViewById(R.id.progress);
-
         this.showSnack();
         return view;
     }
@@ -44,10 +32,5 @@ public class WorkInProgressFragment extends Fragment implements Observer {
                 Snackbar.make(view, "Work In Progress!", Snackbar.LENGTH_SHORT).show();
             }
         });
-    }
-
-    @Override
-    public void update(Observable observable, Object o) {
-        sessionsLoaded = (Boolean) o;
     }
 }
