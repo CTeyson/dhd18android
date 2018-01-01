b07bfc4ec457fd229097e12ab65bfa9a75455d3b
marcozeyen <marco.zeyen@gmail.com>
2018-01-01 18:31:51 +0100

    Changed Url Methods due to problems with ConfTool --> Check Trello.

--- app/src/main/java/com/unikoeln/mazey/dhdexamplesecond/activities/data/http/HttpURL.java
+++ app/src/main/java/com/unikoeln/mazey/dhdexamplesecond/activities/data/http/HttpURL.java
@@ -3,12 +3,12 @@ package com.unikoeln.mazey.dhdexamplesecond.activities.data.http;
 import com.unikoeln.mazey.dhdexamplesecond.activities.utils.Security;
 
 import java.sql.Timestamp;
+import java.util.Date;
 
 // TODO: Remove senstive data like passwords and keys
 
 public class HttpURL {
 
-    private Timestamp timestamp = null;
     private String endpointEventor = null;
     private String endpointC4me = null;
     private String timestampAsString = null;
@@ -27,18 +27,15 @@ public class HttpURL {
     }
 
     // TODO Check why timestamp is behaving strangely
-
     public String getResourceUrlEventor() {
-        timestamp = new Timestamp(System.currentTimeMillis() + 1000000);
-        timestampAsString = String.valueOf(timestamp.getTime());
+        timestampAsString = String.valueOf(System.currentTimeMillis() * 2);
         stringToHashEventor = String.format("%1s%2s", timestampAsString, keyEventor);
         String sha256 = security.getHash(stringToHashEventor);
         return String.format("%1s%2s%3s%4s%5s", endpointEventor, "&nonce=", timestampAsString, "&passhash=", sha256);
     }
 
     public String getResourceUrlC4me() {
-        timestamp = new Timestamp(System.currentTimeMillis() + 10000000);
-        timestampAsString = String.valueOf(timestamp.getTime());
+        timestampAsString = String.valueOf(System.currentTimeMillis() * 2);
         stringToHashC4me = String.format("%1s%2s", timestampAsString, keyC4me);
         String sha256 = security.getHash(stringToHashC4me);
         return String.format("%1s%2s%3s%4s%5s", endpointC4me, "&nonce=", timestampAsString, "&passhash=", sha256);
