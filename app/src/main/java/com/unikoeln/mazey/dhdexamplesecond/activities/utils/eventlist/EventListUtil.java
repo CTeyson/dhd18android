1f61dbbeb74864d15595386f7c92e955e1841f90
marcozeyen <marco.zeyen@gmail.com>
2018-01-17 06:19:52 +0100

    Added new class which is responsible for creating a List<EventItem>.

---
+++ app/src/main/java/com/unikoeln/mazey/dhdexamplesecond/activities/utils/eventlist/EventListUtil.java
@@ -0,0 +1,24 @@
+package com.unikoeln.mazey.dhdexamplesecond.activities.utils.eventlist;
+
+import com.unikoeln.mazey.dhdexamplesecond.activities.data.eventdata.EventItem;
+import com.unikoeln.mazey.dhdexamplesecond.activities.utils.xml.XMLCreater;
+import com.unikoeln.mazey.dhdexamplesecond.activities.utils.xml.XMLParser;
+
+import org.dom4j.Document;
+import org.dom4j.Node;
+
+import java.util.List;
+
+public class EventListUtil {
+
+    public List<EventItem> initList(String content) {
+        List<EventItem> events = null;
+        XMLParser parser = new XMLParser();
+        XMLCreater xml = new XMLCreater();
+        Document document = xml.createXmlDocumentFromString(content);
+        List<Node> nodes = xml.getNodesOfXml(document, "//sessions/session");
+        events = parser.createEventItemListFromLocalFile(nodes);
+        return events;
+    }
+
+}
