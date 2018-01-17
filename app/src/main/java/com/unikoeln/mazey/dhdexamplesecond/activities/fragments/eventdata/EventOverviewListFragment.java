ed860f12f7114d0b688333b94734676ac2365c2e
marcozeyen <marco.zeyen@gmail.com>
2018-01-17 11:24:41 +0100

    Changed AsyncTask. Reason for change is the use of RecyclerView instead of ListView.

--- app/src/main/java/com/unikoeln/mazey/dhdexamplesecond/activities/fragments/eventdata/EventOverviewListFragment.java
+++ app/src/main/java/com/unikoeln/mazey/dhdexamplesecond/activities/fragments/eventdata/EventOverviewListFragment.java
@@ -4,22 +4,24 @@ import android.app.Fragment;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ListView;
 import android.widget.ProgressBar;
 
 import com.unikoeln.mazey.dhdexamplesecond.R;
+import com.unikoeln.mazey.dhdexamplesecond.activities.utils.adapter.EventItemListAdapter;
 import com.unikoeln.mazey.dhdexamplesecond.activities.data.eventdata.EventItem;
-import com.unikoeln.mazey.dhdexamplesecond.activities.utils.adapter.CustomArrayAdapter;
 import com.unikoeln.mazey.dhdexamplesecond.activities.utils.file.FileUtils;
 
 import java.util.List;
 
 public class EventOverviewListFragment extends Fragment {
 
-    private ListView eventData;
+    private RecyclerView eventData;
+    private RecyclerView.LayoutManager layoutManager;
 
     private ProgressBar progressBar;
 
@@ -27,34 +29,57 @@ public class EventOverviewListFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.event_data_overview_fragment, container, false);
-        eventData = view.findViewById(R.id.list_event);
         progressBar = view.findViewById(R.id.progress);
 
-        InitTask initTask = new InitTask();
-        initTask.execute();
+        eventData = view.findViewById(R.id.list_event);
+        layoutManager = new LinearLayoutManager(getContext());
+        eventData.setLayoutManager(layoutManager);
+
+        InitTask task = new InitTask();
+        task.execute();
 
         return view;
     }
 
-    private class InitTask extends AsyncTask<Void, Void, CustomArrayAdapter> {
+    private class InitTask extends AsyncTask<Void, Void, EventItemListAdapter> {
 
         @Override
-        protected CustomArrayAdapter doInBackground(Void... voids) {
+        protected EventItemListAdapter doInBackground(Void... voids) {
 
             FileUtils utils = new FileUtils();
             List<EventItem> events = utils.deserializeEventItemList(getContext());
 
-            return new CustomArrayAdapter(getContext(), events);
+            return new EventItemListAdapter(events, getContext());
         }
 
         @Override
-        protected void onPostExecute(CustomArrayAdapter customArrayAdapter) {
-            super.onPostExecute(customArrayAdapter);
+        protected void onPostExecute(EventItemListAdapter adapter) {
+            super.onPostExecute(adapter);
             progressBar.setVisibility(View.INVISIBLE);
-            eventData.setAdapter(customArrayAdapter);
+            RecyclerView.Adapter mAdapter = adapter;
+            eventData.setAdapter(mAdapter);
         }
-
     }
+
+//    private class InitTask extends AsyncTask<Void, Void, CustomArrayAdapter> {
+//
+//        @Override
+//        protected CustomArrayAdapter doInBackground(Void... voids) {
+//
+//            FileUtils utils = new FileUtils();
+//            List<EventItem> events = utils.deserializeEventItemList(getContext());
+//
+//            return new CustomArrayAdapter(getContext(), events);
+//        }
+//
+//        @Override
+//        protected void onPostExecute(CustomArrayAdapter customArrayAdapter) {
+//            super.onPostExecute(customArrayAdapter);
+//            progressBar.setVisibility(View.INVISIBLE);
+//            //eventData.setAdapter(customArrayAdapter);
+//        }
+//
+//    }
 
 }
 
