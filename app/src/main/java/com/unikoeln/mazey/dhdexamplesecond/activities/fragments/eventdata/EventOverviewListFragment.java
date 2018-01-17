4098d48134a319d8df37498056ba327915ac9975
marcozeyen <marco.zeyen@gmail.com>
2018-01-17 06:15:41 +0100

    Deleted not-needed imports.
    Made field variable View local.
    
    Changed InitTask:
    Refactored Methods.

--- app/src/main/java/com/unikoeln/mazey/dhdexamplesecond/activities/fragments/eventdata/EventOverviewListFragment.java
+++ app/src/main/java/com/unikoeln/mazey/dhdexamplesecond/activities/fragments/eventdata/EventOverviewListFragment.java
@@ -1,7 +1,6 @@
 package com.unikoeln.mazey.dhdexamplesecond.activities.fragments.eventdata;
 
 import android.app.Fragment;
-import android.content.res.AssetManager;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -12,21 +11,14 @@ import android.widget.ListView;
 import android.widget.ProgressBar;
 
 import com.unikoeln.mazey.dhdexamplesecond.R;
-import com.unikoeln.mazey.dhdexamplesecond.activities.data.EventItem;
+import com.unikoeln.mazey.dhdexamplesecond.activities.data.eventdata.EventItem;
 import com.unikoeln.mazey.dhdexamplesecond.activities.utils.adapter.CustomArrayAdapter;
-import com.unikoeln.mazey.dhdexamplesecond.activities.utils.text.Parser;
-import com.unikoeln.mazey.dhdexamplesecond.activities.utils.text.XmlCreater;
+import com.unikoeln.mazey.dhdexamplesecond.activities.utils.file.FileUtils;
 
-import org.apache.commons.io.IOUtils;
-import org.dom4j.Document;
-import org.dom4j.Node;
-
-import java.io.IOException;
 import java.util.List;
 
 public class EventOverviewListFragment extends Fragment {
 
-    private View view;
     private ListView eventData;
 
     private ProgressBar progressBar;
@@ -34,10 +26,9 @@ public class EventOverviewListFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-        view = inflater.inflate(R.layout.event_data_overview_fragment, container, false);
+        View view = inflater.inflate(R.layout.event_data_overview_fragment, container, false);
         eventData = view.findViewById(R.id.list_event);
         progressBar = view.findViewById(R.id.progress);
-
 
         InitTask initTask = new InitTask();
         initTask.execute();
@@ -50,10 +41,10 @@ public class EventOverviewListFragment extends Fragment {
         @Override
         protected CustomArrayAdapter doInBackground(Void... voids) {
 
-            List<EventItem> events = this.initList(this.getAsstes());
-            CustomArrayAdapter customArrayAdapter = new CustomArrayAdapter(getContext(), events);
+            FileUtils utils = new FileUtils();
+            List<EventItem> events = utils.deserializeEventItemList(getContext());
 
-            return customArrayAdapter;
+            return new CustomArrayAdapter(getContext(), events);
         }
 
         @Override
@@ -63,27 +54,7 @@ public class EventOverviewListFragment extends Fragment {
             eventData.setAdapter(customArrayAdapter);
         }
 
-        private String getAsstes() {
-            String content = null;
-            try {
-                AssetManager assetManager = getActivity().getAssets();
-                content = IOUtils.toString(assetManager.open("DHd_Tagung2018_sessions_2017-12-19_14-36-02.xml"), "UTF-8");
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            return content;
-        }
-
-        private List<EventItem> initList(String content) {
-            List<EventItem> events = null;
-            Parser parser = new Parser();
-            XmlCreater xml = new XmlCreater();
-            Document document = xml.createXmlDocumentFromString(content);
-            List<Node> nodes = xml.getNodesOfXml(document, "//sessions/session");
-            events = parser.createEventItemListFromLocalFile(nodes);
-            return events;
-        }
-
     }
 
 }
+
