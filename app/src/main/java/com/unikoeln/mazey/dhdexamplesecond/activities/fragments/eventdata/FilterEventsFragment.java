b2c7de871b0d79558186146fc1e55d7cb0075f82
marcozeyen <marco.zeyen@gmail.com>
2018-01-22 06:26:13 +0100

    Added new Fragment to inflate filter function.

---
+++ app/src/main/java/com/unikoeln/mazey/dhdexamplesecond/activities/fragments/eventdata/FilterEventsFragment.java
@@ -0,0 +1,21 @@
+package com.unikoeln.mazey.dhdexamplesecond.activities.fragments.eventdata;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FilterEventsFragment extends Fragment {
+
+    private View view;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+
+        return view;
+    }
+}
