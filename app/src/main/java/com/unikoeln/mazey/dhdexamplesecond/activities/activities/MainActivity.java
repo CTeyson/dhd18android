33beabbf4305f479c395b099d7a256c2c0a27d8b
marcozeyen <marco.zeyen@gmail.com>
2018-01-22 06:12:10 +0100

    Shortened code.

--- app/src/main/java/com/unikoeln/mazey/dhdexamplesecond/activities/activities/MainActivity.java
+++ app/src/main/java/com/unikoeln/mazey/dhdexamplesecond/activities/activities/MainActivity.java
@@ -69,12 +69,7 @@ public class MainActivity extends AppCompatActivity
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
-
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+        return id == R.id.action_settings || super.onOptionsItemSelected(item);
     }
 
     @SuppressWarnings("StatementWithEmptyBody")
