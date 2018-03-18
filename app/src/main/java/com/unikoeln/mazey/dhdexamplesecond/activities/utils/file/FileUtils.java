851139484464af857d48684af5c5866890f09725
marcozeyen <marco.zeyen@gmail.com>
2018-03-18 11:36:53 +0100

    Added JavaDoc for some methods.

--- app/src/main/java/com/unikoeln/mazey/dhdexamplesecond/activities/utils/file/FileUtils.java
+++ app/src/main/java/com/unikoeln/mazey/dhdexamplesecond/activities/utils/file/FileUtils.java
@@ -15,8 +15,24 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.util.List;
 
+/**
+ * This class is used to provide data which is locally available on the device.
+ */
+
 public class FileUtils {
 
+    /**
+     * Deserializes a file which stores a {@link List} of type {@link EventItem}.</p>
+     * To do so, this class initializes an instance of {@link EventListUtil}. Then, a <i>try/catch-block</i> is used to open a {@link FileInputStream} in order
+     * to retrieve a locally available file in which a {@link List} of type {@link EventItem} is stored.
+     * To retrieve this file the {@link Context} is used: <code>FileInputStream fileInputStream = context.openFileInput("event_item_list.biz");</code>.
+     * In a second step, a new {@link ObjectInputStream} is used in order to read the object from the locally available file and returns it.</p>
+     * In case, the locally file  or the specified class is not available, an {@link IOException} or {@link ClassCastException} is thrown which is handled
+     * in the <i>catch-block</i> of this very method.
+     *
+     * @param context Android Context
+     * @return {@link List} of type {@link EventItem}
+     */
     public List<EventItem> deserializeEventItemList(Context context) {
         EventListUtil eventListUtil = new EventListUtil();
         List<EventItem> tmp = null;
